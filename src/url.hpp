63f461318eeda4888370a2bd3f288d3cb9356a87
Jakub Dolecki <kubus.dolecki@gmail.com>
2023-06-22 18:13:28 +0000

    Removed http::url::hash, because cloudflare strips it apparently

--- src/url.hpp
+++ src/url.hpp
@@ -11,7 +11,7 @@ class url {
 	url(std::string proto, std::string host, std::string uri);
 
 	// As per RFC3986 specification https://datatracker.ietf.org/doc/html/rfc3986
-	// but only for cloudflare hosted sites (eg. port not included)
+	// but only for cloudflare hosted sites (eg. port and hash not included)
 	//
 	//                                                      search
 	//                                              ┌─────────┴────────┐
@@ -19,8 +19,8 @@ class url {
 	//                                              │    │             │
 	//                                path          │    │       ┌───────searchparams["bar"]
 	//                     ┌───────────┴───────────┐│    │       │     │
-	// protocol://hostname/path[0]/path[1]/path[...]?foo=sth&bar=sthelse#hash
-	// └──────────────────────────────────┬─────────────────────────────────┘
+	// protocol://hostname/path[0]/path[1]/path[...]?foo=sth&bar=sthelse
+	// └───────────────────────────────┬───────────────────────────────┘
 	// │                 │               href
 	// └────────┬────────┘
 	//        origin
@@ -39,7 +39,6 @@ class url {
 	const std::string search;
 	const std::unordered_map<std::string, std::string> searchparams; // TODO: create own class
 
-	const std::string hash;
 }; // url
 
 } // namespace http
