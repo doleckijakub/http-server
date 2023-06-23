ee5ad23dba9d339992fbbbf0adef36bf3ab811f2
Jakub Dolecki <kubus.dolecki@gmail.com>
2023-06-23 15:14:00 +0200

    Implemented http::log::info and http::log::warn
    Deleted http::log::error, because it was unused

--- src/log.hpp
+++ src/log.hpp
@@ -4,10 +4,16 @@
 
 namespace http {
 
-template <typename Arg, typename... Args> static void error(Arg &&arg, Args &&...args) {
-	std::cerr << std::forward<Arg>(arg);
-	((std::cerr << std::forward<Args>(args)), ...);
-	std::cerr << std::endl;
+template <typename Arg, typename... Args> static void warn(Arg &&arg, Args &&...args) {
+	std::cout << "warning: " << std::forward<Arg>(arg);
+	((std::cout << std::forward<Args>(args)), ...);
+	std::cout << std::endl;
+}
+
+template <typename Arg, typename... Args> static void info(Arg &&arg, Args &&...args) {
+	std::cout << "info: " << std::forward<Arg>(arg);
+	((std::cout << std::forward<Args>(args)), ...);
+	std::cout << std::endl;
 }
 
 template <typename Arg, typename... Args> static void log(Arg &&arg, Args &&...args) {
